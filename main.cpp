30d339791da633ec3f55aba36511e40d7e83b9d5
dario-MOOC <dariohklug@gmail.com>
2024-06-20 21:48:22 +0000

    Practica serial

--- main.cpp
+++ main.cpp
@@ -6,24 +6,12 @@
 #include "mbed.h"
 
 // Create a DigitalOutput object to toggle an LED whenever data is received.
-static DigitalOut led(LED1);
+// Crea un objeto DigitalOutput para alternar un LED cada vez que se reciben datos.
+DigitalOut led(LED1);
 
 // Create a UnbufferedSerial object with a default baud rate.
-static UnbufferedSerial serial_port(USBTX, USBRX);
-
-void on_rx_interrupt()
-{
-    char c;
-
-    // Toggle the LED.
-    led = !led;
-
-    // Read the data to clear the receive interrupt.
-    if (serial_port.read(&c, 1)) {
-        // Echo the input back to the terminal.
-        serial_port.write(&c, 1);
-    }
-}
+// Crea un objeto UnbufferedSerial con una velocidad en baudios predeterminada.
+UnbufferedSerial serial_port(USBTX, USBRX);
 
 int main(void)
 {
@@ -35,6 +23,15 @@ int main(void)
         /* stop bit */ 1
     );
 
-    // Register a callback to process a Rx (receive) interrupt.
-    serial_port.attach(&on_rx_interrupt, SerialBase::RxIrq);
+    char c;
+
+    while (true) {
+        // Read the data
+        if (serial_port.read(&c, 1)) {
+            // Toggle the LED.
+            led = !led;
+            // Send a # back to the terminal.
+            serial_port.write("#", 1);
+        }
+    }
 }
